3640e454a6a049237c667158fbf7944ec481ea7e
MEERASHRI03 <727723eucs118@skcet.ac.in>
2024-11-06 22:52:46 +0530

    Homepageproject.jsx

---
+++ Project/Routerproject/Homepageproject.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+//import { useNavigate } from "react-router-dom";
+const Homepageproject =()=>
+{
+    
+    return(
+        <div id="main" style={{display:'flex',flexDirection:'column',backgroundImage:"url('https://campuscloud.pk/wp-content/uploads/2023/03/choose-school-management-system-2048x1366.jpeg')",backgroundSize:'cover',backgroundPosition:'left',height:'150px'}}>
+            <header>
+           <div style={{display:'flex'}}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMOTzrqDnayaR8E0jam3k2Y6WhVg8Vme9MX2UnQ7Pr30818YbTzL1ZhnokqIRukFFPuN0&usqp=CAU" style={{height:'45px',width:'45px',borderRadius:'10px',paddingTop:'30px'}}></img>
+           <h2 style={{paddingLeft:'20px',display:'flex',color:'whitesmoke'}}><b><i>Welcome to AcademiaTracker!!!</i></b></h2>
+          
+           <a href="" style={{paddingTop:'20px',paddingRight:'20px',paddingLeft:'450px',color:'white'}}>Home</a>
+           <a href="#two" style={{paddingTop:'20px',paddingRight:'20px',color:'white'}}>About us</a>
+           <a href="StudentManagementPage" style={{paddingTop:'20px',paddingRight:'20px',color:'white'}}>Create Profile</a>
+           <a href="aboutus.js" style={{paddingTop:'20px',paddingRight:'20px',color:'white'}}>Users</a>
+           <a href="aboutus.js" style={{paddingTop:'20px',paddingRight:'20px',color:'white'}}>Generate sheets</a>
+           <a href="Contactus" style={{paddingTop:'20px',paddingRight:'20px',color:'white'}}>Contact& Feedback</a> 
+           <div style={{paddingRight:'15px',paddingTop:'19px'}}>
+           <a href="Signinpage" style={{color:'white'}}>Login Page</a>
+            </div>  
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Eo_circle_deep-purple_white_letter-m.svg/480px-Eo_circle_deep-purple_white_letter-m.svg.png' alt='myImage' style={{height:'50px',paddingTop:'20px',paddingRight:'20px'}}></img>
+           </div>
+           </header>
+           <br></br>
+           <main>
+                <div id="one" style={{display:'flex',flexDirection:'row'}}>
+                    <div style={{backgroundColor:'#3944BC',height:'350px',width:'700px',color:'whitesmoke'}}>
+                        <br></br>
+                    <h1 style={{paddingLeft:'120px'}}> Student Profile Software</h1>
+                    <p style={{paddingLeft:'100px'}}>Effortlessly manage pupil data, track academic progress and personalise learning with Academia Tracker.1 Lakh+ students and Administrations trust us!<br></br> The best student profile management software.</p>
+                    <br></br>
+                    <div style={{paddingLeft:'230px'}}>
+                    <button style={{color:'black',height:'50px',width:'120px',borderRadius:'15px'}}>Learn More</button>
+                    </div>
+                    </div>
+                    <div>
+                        <img src="https://educationhorizons.com/wp-content/uploads/2024/03/img-flexible-categorisation.webp" style={{height:'350px',width:'600px'}}></img>
+                    </div>
+                </div>
+                <br></br>
+                <hr style={{height: '1px', backgroundColor: 'black', width: '900px'}} />
+                <div id="two">
+                    <h1 style={{textAlign:'center',color:'black'}}>About Us </h1>
+                    <div style={{borderRadius:'15px'}}>
+                        <p style={{paddingLeft:'30px'}}>Academia Tracker, the top student profile management software, is a versatile and user-friendly tool designed to streamline educational data management.Tailored for educators and <br></br>administration, this student information system software offers a comprehensive solution for tracking and analysing student information, including academic performance, <br></br>attendance, and behavioural insights.
+                         <br></br><br></br>With the best student profile management software features, Academia Tracker fosters personalised learning experiences and encourages informed decision-making. It enhances overall<br></br> engagement with classroom teaching by providing special aid in evaluating grades and advising members of the faculty on how to improve their teaching skills.
+                         <br></br><br></br>The implementation of Academia Tracker's student profile management system improves education by serving as an essential source of <br></br>accurate information that caters to everyone’s needs within the school community.</p>
+                    </div>
+                </div>
+                <br></br>
+                <hr style={{height: '1px', backgroundColor: 'black', width: '900px'}} />
+                <br></br>
+                <div id="three" style={{display:'flex',flexDirection:'row'}}>
+                    <div style={{height:'350px',width:'700px',color:'black'}}>
+                        <br></br>
+                    <h1 style={{paddingLeft:'110px'}}>  Efficient data management</h1>
+                    <p style={{paddingLeft:'40px'}}>Academia Tracker users benefit from quick, specific searches for student information, <br></br>allowing tailored queries based on specified criteria. This accuracy ensures rapid <br></br> access to essential student details, crucial for busy administrators and educators.
+                    <br></br><br></br>
+                    Academia Tracker student profile software centralises pupil data, enhancing organisation and accessibility.This efficiency saves time and improves reliability in managing student's data.
+                    </p>
+                    
+                    </div>
+                    <div>
+                        <img src="https://educationhorizons.com/wp-content/uploads/2024/03/img-custom-fields.webp" style={{height:'350px',width:'600px'}}></img>
+                    </div>
+                </div>
+                <br></br>
+                <hr style={{height: '1px', backgroundColor: 'black', width: '900px'}} />
+                <br></br>
+                <div id="four" style={{display:'flex',flexDirection:'row'}}>
+                    <div>
+                        <img src="https://educationhorizons.com/wp-content/uploads/2024/03/Engage-Solutions-Comprehensive-Record-Keeping.webp" style={{height:'350px',width:'600px'}}></img>
+                    </div>
+                    <div style={{height:'350px',width:'700px',color:'black'}}>
+                        <br></br>
+                    <h1 style={{paddingLeft:'110px'}}> Comprehensive record-keeping</h1>
+                    <p style={{paddingLeft:'40px'}}>Beyond basic information, Academia Tracker records additional data like medical records and transport details. This comprehensive overview aids informed decision-making and ensures holistic student profile management.
+                    <br></br><br></br>
+                    Real-time updates and easy information sharing foster collaboration among stakeholders, ensuring well-informed communities within educational institutions.
+                    </p> 
+                    </div>
+                </div>
+                <br></br>
+                <hr style={{height: '1px', backgroundColor: 'black', width: '900px'}} />
+                <br></br>
+                <div id="five" style={{display:'flex',flexDirection:'row'}}>
+                    <div style={{height:'350px',width:'700px',color:'black'}}>
+                        <br></br>
+                    <h1 style={{paddingLeft:'110px'}}>  Student profile features</h1>
+                    <p style={{paddingLeft:'40px'}}>Student profiles encompass a diverse range of information crucial to understanding each student’s academic journey, evolving over time to offer a nuanced perspective on progress and achievements.
+                    <br></br><br></br>
+                    This dynamic digital record serves as a valuable tool for educators and administrators in tracking student development and providing targeted support where needed.
+                    </p>
+                    
+                    </div>
+                    <div>
+                        <img src="https://educationhorizons.com/wp-content/uploads/2024/03/img-student-profile-features.webp" style={{height:'350px',width:'600px'}}></img>
+                    </div>
+                </div>
+               </main>
+
+        </div>
+    )
+}
+export default Homepageproject;
